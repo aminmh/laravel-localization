cccae74ffa2b2e538b9526f9d87cae664c2becd0
Mohammad Ebrahim <aminmh654@gmail.com>
2023-02-25 20:22:25 +0330

    Feature/migrator command output (#14)
    * feature: add ConsoleOutput
    
    * refactor: refactor array writer response
    
    ---------
    
    Co-authored-by: mohammad.m <mohammad.m@bugloos.com>

---
+++ src/Responses/SuccessMigratorResponse.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Bugloos\LaravelLocalization\Responses;
+
+use Bugloos\LaravelLocalization\Views\Console\ConsoleOutput;
+
+class SuccessMigratorResponse implements \Stringable
+{
+    public function __construct(
+        public readonly string $label,
+        public readonly string $category,
+        public readonly string $translate,
+        public readonly string $locale
+    ) {
+    }
+
+    public function __toString(): string
+    {
+        return ConsoleOutput::writeLn(sprintf(
+            "Label %s from category %s translate to %s with %s language.",
+            $this->label,
+            $this->category,
+            $this->translate,
+            $this->locale
+        ), ConsoleOutput::CONSOLE_TEXT_GREEN);
+    }
+}
