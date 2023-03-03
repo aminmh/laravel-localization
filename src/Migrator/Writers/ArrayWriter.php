a23e45b3fd45caaef1997dbd445a8c38d7233297
mohammad.m <mohammad.m@bugloos.com>
2023-03-03 16:57:59 +0330

    feature: add lcalization own exception class

--- src/Migrator/Writers/ArrayWriter.php
+++ src/Migrator/Writers/ArrayWriter.php
@@ -4,6 +4,7 @@ namespace Bugloos\LaravelLocalization\Migrator\Writers;
 
 use Bugloos\LaravelLocalization\Abstract\AbstractWriter;
 use Bugloos\LaravelLocalization\Contracts\LazyPersistsWriteInterface;
+use Bugloos\LaravelLocalization\Exceptions\LocalizationResourceException;
 use Bugloos\LaravelLocalization\Responses\FailedMigratorResponse;
 use Bugloos\LaravelLocalization\Responses\SuccessMigratorResponse;
 use Illuminate\Database\QueryException;
@@ -12,20 +13,23 @@ class ArrayWriter extends AbstractWriter implements LazyPersistsWriteInterface
 {
     public function save(): \Generator
     {
-        $categoryObject = static::$translator->addCategory($category = $this->reader->getCategory());
+        try {
+            $categoryObject = static::$translator->addCategory($category = $this->reader->getCategory());
 
-        $locale = $this->reader->getLocale();
+            $locale = $this->reader->getLocale();
 
-        foreach ($this->reader->getContent() as $label => $translate) {
-            try {
+            foreach ($this->reader->getContent() as $label => $translate) {
                 $labelObject = static::$translator->addLabel($label, $categoryObject);
 
                 $translated = static::$translator->translate($labelObject, $translate, $locale);
 
                 yield (new SuccessMigratorResponse($translated));
-            } catch (QueryException $ex) {
-                yield (new FailedMigratorResponse($label, $category, $translate, $locale));
             }
+        } catch (LocalizationResourceException $ex) {
+            yield (new FailedMigratorResponse($label, $category, $translate, $locale));
         }
+//        catch (QueryException $ex) {
+//            yield (new FailedMigratorResponse($label, $category, $translate, $locale));
+//        }
     }
 }
